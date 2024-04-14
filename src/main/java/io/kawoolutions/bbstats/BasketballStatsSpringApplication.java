b3662f2b3563660467fe99773a2f8a8223e3e05d
kʰavu'luːʃənz <info@kawoolutions.com>
2024-04-14 12:41:41 +0200

    Added default Spring Boot application stuff

---
+++ src/main/java/io/kawoolutions/bbstats/BasketballStatsSpringApplication.java
@@ -0,0 +1,13 @@
+package io.kawoolutions.bbstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballStatsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballStatsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kawoolutions/bbstats/BasketballStatsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kawoolutions.bbstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballStatsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
