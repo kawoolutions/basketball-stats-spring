5011d39a8a3664c9c5c258ee7e83e19c223d9326
kʰavu'luːʃənz <info@kawoolutions.com>
2024-04-16 11:55:39 +0200

    ContactTypeConverter added

---
+++ src/main/java/io/kawoolutions/bbstats/entity/converter/ContactTypeConverter.java
@@ -0,0 +1,50 @@
+package io.kawoolutions.bbstats.entity.converter;
+
+import io.kawoolutions.bbstats.entity.ContactType;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import static io.kawoolutions.bbstats.entity.ContactType.ARENA;
+import static io.kawoolutions.bbstats.entity.ContactType.CLUB;
+import static io.kawoolutions.bbstats.entity.ContactType.PERSON;
+
+@Converter(autoApply = false)
+public class ContactTypeConverter implements AttributeConverter<ContactType, String> {
+    @Override
+    public String convertToDatabaseColumn(ContactType contactType) {
+//        System.out.println( "convertToEntityAttribute(" + contactType + ")" );
+
+        switch (contactType) {
+            case PERSON:
+                return "P";
+
+            case CLUB:
+                return "C";
+
+            case ARENA:
+                return "A";
+
+            default:
+                throw new IllegalArgumentException("Unknown enum value: " + contactType);
+        }
+    }
+
+    @Override
+    public ContactType convertToEntityAttribute(String value) {
+//        System.out.println( "convertToEntityAttribute(\"" + value + "\")" );
+
+        switch (value) {
+            case "P":
+                return PERSON;
+
+            case "C":
+                return CLUB;
+
+            case "A":
+                return ARENA;
+
+            default:
+                throw new IllegalArgumentException("Unknown DB value: " + value);
+        }
+    }
+}
