3a1bc32996aea36c1359a7983dac3193e9fa44af
kʰavu'luːʃənz <info@kawoolutions.com>
2024-04-21 22:12:28 +0200

    Setup DB init scripts for H2 (not working)

--- src/test/java/io/kawoolutions/bbstats/BasketballStatsSpringApplicationTests.java
+++ src/test/java/io/kawoolutions/bbstats/BasketballStatsSpringApplicationTests.java
@@ -2,8 +2,10 @@ package io.kawoolutions.bbstats;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+@Sql({"/schema.sql", "/data.sql"})
 class BasketballStatsSpringApplicationTests {
 
     @Test
