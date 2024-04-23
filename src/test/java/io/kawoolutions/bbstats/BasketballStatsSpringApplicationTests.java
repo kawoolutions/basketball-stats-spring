dc50feb8f9f5401e2d89c1f25d376504b74ce3ba
kʰavu'luːʃənz <info@kawoolutions.com>
2024-04-23 16:26:23 +0200

    Work in progress

--- src/test/java/io/kawoolutions/bbstats/BasketballStatsSpringApplicationTests.java
+++ src/test/java/io/kawoolutions/bbstats/BasketballStatsSpringApplicationTests.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.kawoolutions.bbstats.entity.Continent;
+import io.kawoolutions.bbstats.entity.Season;
 import io.kawoolutions.bbstats.repository.ContinentRepository;
+import io.kawoolutions.bbstats.repository.SeasonRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,10 +17,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BasketballStatsSpringApplicationTests {
 
     @Autowired
+    private SeasonRepository seasonRepository;
+
+    @Autowired
     private ContinentRepository continentRepository;
 
     @Test
-    void contextLoads() {
+    void testDatabaseCreation() {
+    }
+
+    @Test
+    void testDatabaseSeasons() {
+        List<Season> seasons = seasonRepository.findAll();
+        assertEquals(seasons.size(), 5);
+    }
+
+    @Test
+    void testDatabaseContinents() {
         List<Continent> continents = continentRepository.findAll();
         assertEquals(continents.size(), 7);
     }
