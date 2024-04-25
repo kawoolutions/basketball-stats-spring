2ac474efa9bf7e914c3fecc1928b39b7f57e56a0
kʰavu'luːʃənz <info@kawoolutions.com>
2024-04-25 13:31:55 +0200

    Removed unused import

--- src/test/java/io/kawoolutions/bbstats/ApplicationUnitTests.java
+++ src/test/java/io/kawoolutions/bbstats/ApplicationUnitTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
