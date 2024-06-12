38fce9fed8ab1c0771e7cc90628b1aa5fcbace08
kʰavu'luːʃənz <info@kawoolutions.com>
2024-06-12 14:47:19 +0200

    Removed unused imports

--- src/main/java/io/kawoolutions/bbstats/rest/SeasonResource.java
+++ src/main/java/io/kawoolutions/bbstats/rest/SeasonResource.java
@@ -1,18 +1,15 @@
 package io.kawoolutions.bbstats.rest;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
